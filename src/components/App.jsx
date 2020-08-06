1c0061d311b8f60fe7d2135ef9ad30f046ebe46f
Darryl Cousins <darryljcousins@gmail.com>
2020-08-07 11:54:49 +1200

    remove debug buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -185,7 +185,6 @@ export default function App({ shopifyId }) {
                   position: 'relative',
                 }}
                 >
-                  { testMarkup }
                   <Spacer />
                   <DateSelect
                     boxes={boxes}
